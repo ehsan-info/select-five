0452444a71cb2c5695055145c52f578e6b4a0860
ehsan-info <ehsanurrahman1992@gmail.com>
2022-08-22 21:43:15 +0300

    calculation field edited

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+function getInputValue(elementId) {
+    const getValuePrice = document.getElementById(elementId);
+    const getValuePriceString = getValuePrice.value;
+    const getPrice = parseInt(getValuePriceString);
+    return getPrice;
+}
+function getTextValue(textId) {
+    const getPlayerValue = document.getElementById(textId);
+    const getPlayerValueString = getPlayerValue.innerText;
+    const newPlayerValue = parseInt(getPlayerValueString);
+    return newPlayerValue;
+}
 document.getElementById('btn-calculate').addEventListener('click', function () {
-    console.log('clicked')
-})
+    const getPlayerPrice = getInputValue('player-price');
+    const playerExpenses = document.getElementById('player-expenses');
+    const newPlayerExpenses = 5 * getPlayerPrice;
+    playerExpenses.innerText = newPlayerExpenses;
+});
+document.getElementById('btn-total').addEventListener('click', function () {
+    const getManagerPrice = getInputValue('manager-price');
+    const getCoachPrice = getInputValue('coach-price');
+    const getPlayerExpenses = getTextValue('player-expenses');
+    const totalPrice = getPlayerExpenses + getManagerPrice + getCoachPrice;
+    const getTotalPrice = document.getElementById('total-expenses');
+    getTotalPrice.innerText = totalPrice;
+});
