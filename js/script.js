662b1e547dcdeaa25bb1ea8ea5b1d18a355001ba
ehsan-info <ehsanurrahman1992@gmail.com>
2022-08-23 11:36:39 +0300

    player name added to ordered list

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 function getInputValue(elementId) {
     const getValuePrice = document.getElementById(elementId);
     const getValuePriceString = getValuePrice.value;
-    const getPrice = parseInt(getValuePriceString);
-    return getPrice;
+    if (/[a-zA-Z]/.test(getValuePriceString)) {
+        alert('Please enter only number');
+        return;
+    }
+    else {
+        const getPrice = parseInt(getValuePriceString);
+        return getPrice;
+    }
+
 }
 function getTextValue(textId) {
     const getPlayerValue = document.getElementById(textId);
@@ -10,11 +17,31 @@ function getTextValue(textId) {
     const newPlayerValue = parseInt(getPlayerValueString);
     return newPlayerValue;
 }
+function getPlayerName(playerId) {
+    const getPlayerName = document.getElementById(playerId);
+    const getPlayerNameString = getPlayerName.innerText;
+    const playerTotalList = document.getElementById('player-total-list');
+    const playerList = document.getElementsByClassName('item');
+    if (playerList.length + 1 > 5) {
+        alert('No more than 5 players');
+        return;
+    }
+    const li = document.createElement('li');
+    li.innerText = getPlayerNameString;
+    li.classList.add('item');
+    playerTotalList.appendChild(li);
+}
 document.getElementById('btn-calculate').addEventListener('click', function () {
     const getPlayerPrice = getInputValue('player-price');
     const playerExpenses = document.getElementById('player-expenses');
-    const newPlayerExpenses = 5 * getPlayerPrice;
-    playerExpenses.innerText = newPlayerExpenses;
+    const playerList = document.getElementsByClassName('item');
+    if (typeof getPlayerPrice === 'undefined') {
+        playerExpenses.innerText = 00;
+    }
+    else {
+        const newPlayerExpenses = playerList.length * getPlayerPrice;
+        playerExpenses.innerText = newPlayerExpenses;
+    }
 });
 document.getElementById('btn-total').addEventListener('click', function () {
     const getManagerPrice = getInputValue('manager-price');
