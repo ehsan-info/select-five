6fca5e00f6bcccf87e2a686b01a07cacf8ea975f
ehsan-info <ehsanurrahman1992@gmail.com>
2022-08-23 11:38:06 +0300

    common script has been placed in common js file

--- js/script.js
+++ js/script.js
@@ -1,36 +1,3 @@
-function getInputValue(elementId) {
-    const getValuePrice = document.getElementById(elementId);
-    const getValuePriceString = getValuePrice.value;
-    if (/[a-zA-Z]/.test(getValuePriceString)) {
-        alert('Please enter only number');
-        return;
-    }
-    else {
-        const getPrice = parseInt(getValuePriceString);
-        return getPrice;
-    }
-
-}
-function getTextValue(textId) {
-    const getPlayerValue = document.getElementById(textId);
-    const getPlayerValueString = getPlayerValue.innerText;
-    const newPlayerValue = parseInt(getPlayerValueString);
-    return newPlayerValue;
-}
-function getPlayerName(playerId) {
-    const getPlayerName = document.getElementById(playerId);
-    const getPlayerNameString = getPlayerName.innerText;
-    const playerTotalList = document.getElementById('player-total-list');
-    const playerList = document.getElementsByClassName('item');
-    if (playerList.length + 1 > 5) {
-        alert('No more than 5 players');
-        return;
-    }
-    const li = document.createElement('li');
-    li.innerText = getPlayerNameString;
-    li.classList.add('item');
-    playerTotalList.appendChild(li);
-}
 document.getElementById('btn-calculate').addEventListener('click', function () {
     const getPlayerPrice = getInputValue('player-price');
     const playerExpenses = document.getElementById('player-expenses');
