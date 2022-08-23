6f8a40ec733d5194aeccf38016b198456f9e0750
ehsan-info <ehsanurrahman1992@gmail.com>
2022-08-23 13:04:04 +0300

    Button disable and commenting part commited

--- js/common.js
+++ js/common.js
@@ -1,22 +1,25 @@
+//Get Input Value Common Function
 function getInputValue(elementId) {
     const getValuePrice = document.getElementById(elementId);
     const getValuePriceString = getValuePrice.value;
     if (/[a-zA-Z]/.test(getValuePriceString)) {
         alert('Please enter only number');
+        getValuePrice.value = '';
         return;
     }
     else {
         const getPrice = parseInt(getValuePriceString);
         return getPrice;
     }
-
 }
-function getTextValue(textId) {
+//Get Input Field Text Value Common Fucntion
+function getInputTextValue(textId) {
     const getPlayerValue = document.getElementById(textId);
     const getPlayerValueString = getPlayerValue.innerText;
     const newPlayerValue = parseInt(getPlayerValueString);
     return newPlayerValue;
 }
+//Get Player Name list Function
 function getPlayerName(playerId) {
     const getPlayerName = document.getElementById(playerId);
     const getPlayerNameString = getPlayerName.innerText;
--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
+//Calculation Procedures
 document.getElementById('btn-calculate').addEventListener('click', function () {
     const getPlayerPrice = getInputValue('player-price');
     const playerExpenses = document.getElementById('player-expenses');
     const playerList = document.getElementsByClassName('item');
-    if (typeof getPlayerPrice === 'undefined') {
-        playerExpenses.innerText = 00;
+    if (typeof getPlayerPrice === 'undefined' || playerList.length === 0) {
+        playerExpenses.innerText = 000;
     }
     else {
         const newPlayerExpenses = playerList.length * getPlayerPrice;
         playerExpenses.innerText = newPlayerExpenses;
     }
 });
+
+//Total Calculation Procedures
 document.getElementById('btn-total').addEventListener('click', function () {
     const getManagerPrice = getInputValue('manager-price');
     const getCoachPrice = getInputValue('coach-price');
-    const getPlayerExpenses = getTextValue('player-expenses');
-    const totalPrice = getPlayerExpenses + getManagerPrice + getCoachPrice;
+    const getPlayerExpenses = getInputTextValue('player-expenses');
     const getTotalPrice = document.getElementById('total-expenses');
-    getTotalPrice.innerText = totalPrice;
+    if (getManagerPrice === 'NaN' || getCoachPrice === 'NaN' || getPlayerExpenses === 0) {
+        getTotalPrice.innerText = 00;
+    }
+    else {
+        const totalPrice = getPlayerExpenses + getManagerPrice + getCoachPrice;
+        getTotalPrice.innerText = totalPrice;
+    }
 });
